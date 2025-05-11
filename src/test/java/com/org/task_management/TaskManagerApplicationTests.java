ddc040f29bd3844b911a959a3ccc59775af4329f
Dalia <d.ahmed2112@gmail.com>
2025-05-12 02:57:58 +0300

    initial spring boot setup

---
+++ src/test/java/com/org/task_management/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
