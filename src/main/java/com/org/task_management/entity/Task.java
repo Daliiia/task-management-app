10bb4208d11979e911d3baaaab6d0085983db6e6
Dalia <d.ahmed2112@gmail.com>
2025-05-25 19:23:24 +0300

    import  NoArgsConstuctor Lombok

--- src/main/java/com/org/task_management/entity/Task.java
+++ src/main/java/com/org/task_management/entity/Task.java
@@ -3,6 +3,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
